b6d378071a623ab109206f9fdf739ce06b49db4d
tuanha1305 <tuancntt971@gmail.com>
2024-04-04 17:07:47 +0700

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [{
+    name: 'Telegram Download Multi Channel',
+    script: 'app.py',
+    interpreter: 'python3',
+    autorestart: false,
+    watch: false,
+  }],
+};
